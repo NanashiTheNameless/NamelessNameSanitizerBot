708143066f20a4e4cd782d74e02610c80002ada2
NamelessNanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-12-01 17:05:49 -0600

    update

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,4 +1,5 @@
 # This software is licensed under NNCL v1.2 see LICENSE.md for more info
+# https://github.com/NanashiTheNameless/NamelessNameSanitizerBot/blob/main/LICENSE.md
 """NamelessNameSanitizerBot package.
 
 This file marks the 'bot' directory as a Python package so that
--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,5 @@
 # This software is licensed under NNCL v1.2 see LICENSE.md for more info
+# https://github.com/NanashiTheNameless/NamelessNameSanitizerBot/blob/main/LICENSE.md
 """
 SanitizerBot Discord client implementation.
 
